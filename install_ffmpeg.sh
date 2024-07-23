886f6c191c81244a6cc0c00cae6557b379a8e4f0
NB <nasrobegg@gmail.com>
2024-07-23 23:28:54 +0100

    Update install_ffmpeg.sh

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-
-set -eux
-
-# Install FFmpeg
-apt-get update
-apt-get install -y ffmpeg
-
-# Install Python dependencies
-pip install -r requirements.txt
+# Download the latest static build of FFmpeg for Linux
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+# Extract the archive
+tar -xvf ffmpeg-release-amd64-static.tar.xz
+# Create a directory to store the binaries
+mkdir -p ffmpeg
+# Move the binaries to the ffmpeg directory
+mv ffmpeg-*-amd64-static/ffmpeg ffmpeg/
+mv ffmpeg-*-amd64-static/ffprobe ffmpeg/
+# Set executable permissions
+chmod +x ffmpeg/ffmpeg
+chmod +x ffmpeg/ffprobe
+# Clean up
+rm -rf ffmpeg-*-amd64-static
+rm ffmpeg-release-amd64-static.tar.xz
